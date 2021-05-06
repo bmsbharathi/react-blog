79f4d148bbd86bb673c6f2870d4ea04e8e28e659
Bharathi BMS <bharathi.bms@thoughtworks.com>
2021-05-06 19:59:00 +0530

    Fix components alignment in landing page

--- src/Posts.js
+++ src/Posts.js
@@ -4,6 +4,11 @@ import './css/posts.css';
 
 const Posts = ({posts}) => {
     
+    const deletePost = (evt) => {
+
+        console.log("Delete Post "+ evt.target);
+    };
+
     return ( 
         <div className="posts">
             <h2>All Posts</h2>
@@ -12,10 +17,11 @@ const Posts = ({posts}) => {
                     <div className="post-preview" key={ post.id } >
                         <Link to={ `/posts/${post.id}` }>
                             <h3>{ post.title } </h3>
-                            <p align="left">Written By: { post.author } </p>
-                            <p align="left">Created: { humanized_time_span(post.createdOn) }</p>
+                            <p align="left"><b>Written By:</b> { post.author } </p> 
+                            <p align="left"><b>Created:</b> { humanized_time_span(post.createdOn) }</p>
                         </Link>
                     </div>
+                          
                     )
                 )
              }
